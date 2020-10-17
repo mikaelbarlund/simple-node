1a5f7f0ce532482dbf17ee50a671248d7aa5636d
Mikael Bärlund <midas@iki.fi>
2020-10-17 20:43:01 +0300

    update index

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var express = require('express')
-var app = express()
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-app.listen(8080, function () {
-  console.log('app listening on port 8080!')
-})
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.send('Hello World!'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
